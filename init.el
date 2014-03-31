268f65d11c4cd99b452215f7f063936ac998b4a3
jiangtao <jiangtao@tao-studio.net>
2014-03-31 11:06:16 +0800

    change start directory name

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 ;; -*- Emacs-Lisp -*-
-;; Time-stamp: <2014-03-22 14:02:19 by jiangtao>
+;; Time-stamp: <2014-03-31 11:05:59 by jiangtao>
 
 (setq user-mail-address "jiangtao@tao-studio.net")
 (setq user-full-name    "Stone Jiang")
 
 
-(defconst my-emacs-path "/Users/jiangtao/.emacs.d/" "我的emacs相关配置文件的路径")
+(defconst my-emacs-path "~/.emacs.d/" "我的emacs相关配置文件的路径")
 (defconst my-emacs-my-lisps-path  (concat my-emacs-path "my-lisps/") "我自己写的emacs lisp包的路径")
 (defconst my-emacs-lisps-path     (concat my-emacs-path "lisps/") "我下载的emacs lisp包的路径")
 (defconst my-emacs-templates-path (concat my-emacs-path "templates/") "Path for templates")
