02aafa79d028c3ce9890af05c501b67078470073
jiangtao <jiangtao@tao-studio.net>
2015-01-17 15:17:48 +0800

    add smail

--- my-lisps/init-dev.el
+++ my-lisps/init-dev.el
@@ -59,3 +59,6 @@
 ;;; init-dev.el ends here
 
 (require 'android-mode)
+
+(autoload 'smali-mode "smali-mode" "Major mode for editing and viewing smali issues" t)
+(add-to-list 'auto-mode-alist '(".smali$" . smali-mode))
